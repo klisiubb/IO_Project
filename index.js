8e6da5323b92975280d7ad03b0b970a68f78704f
klisiubb <76220346+klisiubb@users.noreply.github.com>
2021-11-08 17:07:02 +0100

    Added Bootstrap to project
    Added Bootstrap 5 to project and set /public folder

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.set('view engine', 'hbs');
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
