91528c2e5bc12083d7744bf6cdb22bd21f08284b
klisiubb <76220346+klisiubb@users.noreply.github.com>
2021-11-24 13:16:45 +0100

    Changes to index.js and dependency adding
    Changed index.js to current state of project.

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3000
-require('dotenv').config()
-app.set('view engine', 'hbs');
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const app = express();
+const expressEjsLayout = require('express-ejs-layouts')
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require("passport");
+const path = require('path')
 app.use(express.static('public'))
-var mongoose = require("mongoose");
-var passport = require("passport");
-var bodyParser = require("body-parser");
-var LocalStrategy = require("passport-local");
-var passportLocalMongoose = require("passport-local-mongoose");
-var User = require("./models/users");
+//passport config:
+require('./config/passport')(passport)
+//mongoose
+mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
+.then(() => console.log('connected,,'))
+.catch((err)=> console.log(err));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(require("express-session")({
-secret: "plx2021",
-resave: false,
-saveUninitialized: false
+//EJS
+app.set('view engine','ejs');
+//BodyParser
+app.use(express.urlencoded({extended : false}));
+//express session
+app.use(session({
+    secret : 'secret',
+    resave : true,
+    saveUninitialized : true
 }));
-
-//passport
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+app.use(flash());
+app.use((req,res,next)=> {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error  = req.flash('error');
+    next();
+    })
+    
+//Routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
 
-// end of test code
-app.listen(port, () => {
-  console.log(`Portal PLX działa na porcie: ${port}`)
-})
+app.listen(3000);
