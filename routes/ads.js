cbc960e18eedaede53699c7dad9da41e30097618
BartShoot <33185107+BartShoot@users.noreply.github.com>
2021-12-01 18:40:34 +0100

    Ulepszenie dodawania ogłoszeń

--- routes/ads.js
+++ routes/ads.js
@@ -11,7 +11,29 @@ router.get('/add', (req,res)=>{
 
 //<!---POST an ad ---!>
 router.post('/add', (req,res)=>{
-    //TODO
+    const {title,description, category_id,phone,photo1} = req.body;
+    let errors = [];
+    if(!title || !description || !phone || !photo1 || !category_id) {
+        errors.push({msg : "Wypełnij wszystkie pola."})
+    }
+    if(description.length < 40 ) {
+        errors.push({msg : 'Opis ogłoszenia musi miec więcej niż 20 znaków'})
+    }
+    if(title.length < 10 ) {
+        errors.push({msg : 'Tytul ogłoszenia musi miec więcej niż 10 znaków'})
+    }
+    if(errors.length > 0 ) {
+        res.render('add', {
+            errors : errors,
+            title : title,
+            description : description,
+            phone : phone,
+            category_id : category_id,
+            photo1: photo1
+        })
+         } else {
+             //todo
+         }
 })
 //<!--- report  an ad POST ---!>
 router.post('/report', (req,res)=>{
