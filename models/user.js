73f34dfe888812e63f75318176fa59fd7748f738
BartShoot <33185107+BartShoot@users.noreply.github.com>
2021-12-01 18:42:24 +0100

    Przygotowanie do tworzenia kont moderatorów

--- models/user.js
+++ models/user.js
@@ -21,6 +21,14 @@ profilepic :{
     required : false,
     default: "https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-user-interface-kiranshastry-gradient-kiranshastry.png",
 } ,
+moderator:{
+  type: Boolean, // to check if user is an moderator, to access moderate functions
+  default: false  
+},
+ad_limit:{
+ type: Number, // limit of ads per user, may be changed in the future for $$$
+ default: 3
+}
 });
 const User= mongoose.model('User',UserSchema);
 
