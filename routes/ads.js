c016146ef1ab140a4c11f63ad3d1f285532fd68e
BartShoot <33185107+BartShoot@users.noreply.github.com>
2021-12-01 18:39:32 +0100

    Dodawanie ogłoszenia

--- routes/ads.js
+++ routes/ads.js
@@ -3,3 +3,27 @@ const router = express.Router();
 
 const ad = require("../models/ad");
 const report = require("../models/report");
+
+//<!--- Add an ad ---!>
+router.get('/add', (req,res)=>{
+    res.render('add');
+})
+
+//<!---POST an ad ---!>
+router.post('/add', (req,res)=>{
+    //TODO
+})
+//<!--- report  an ad POST ---!>
+router.post('/report', (req,res)=>{
+    res.send('Reported an ad.Thank you!');
+    //TODO
+})
+//<!--- edit an ad ---!>
+router.get('/edit', (req,res)=>{
+    res.render('edit');
+})
+
+//<!--- edit POST ---!>
+router.post('/edit', (req,res)=>{
+    //TODO
+})
