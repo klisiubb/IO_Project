11392c0697be74418a823f0cdf200eb675cd07d6
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2021-12-15 14:40:44 +0100

    Update send_email.js
    copied functions to send email with temp password and to verify email

--- functions/send_email.js
+++ functions/send_email.js
@@ -4,7 +4,7 @@ var nodemailer = require('nodemailer');
 
 function sendEmailAfterRegister(userEmail) {
     var transporter = nodemailer.createTransport({
-        service: 'gmail',
+        service: process.env.EMAIL_SERVICE,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_USER
@@ -15,6 +15,54 @@ function sendEmailAfterRegister(userEmail) {
         from: process.env.EMAIL_USER,
         to: userEmail,
         subject: 'PLX  - Witamy na portalu PLX',
+        text: 'Dziękujemy za rejestrację' // todo fancy text here
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      }); 
+}
+function sendEmailAfterPasswordChange(userEmail,newTempPassword) {
+    var transporter = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_USER
+        }
+      });
+      
+      var mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: userEmail,
+        subject: 'PLX  - zmiana hasła',
+        text: 'Twoje nowe hasło to :'+newTempPassword +' zmien je zaraz po zalogowaniu sie!' // todo fancy text here
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      }); 
+}
+function sendEmailToVerify(userEmail,verifyLink) {
+    var transporter = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_USER
+        }
+      });
+      
+      var mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: userEmail,
+        subject: 'PLX  - weryfikacja adresu email',
         text: 'Dziękujemy za rejestrację' // todo fancy text here
       };
       
