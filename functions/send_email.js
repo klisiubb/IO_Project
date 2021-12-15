b341511609be3cb86aeace6cd361d383f640abe7
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2021-12-15 15:01:17 +0100

    Update send_email.js
    +emailNewMessage
    +emailNewProductFromStarredCategory

--- functions/send_email.js
+++ functions/send_email.js
@@ -50,7 +50,7 @@ function sendEmailAfterPasswordChange(userEmail,newTempPassword) {
         }
       }); 
 }
-function sendEmailToVerify(userEmail,verifyLink) {
+function sendEmailToVerify(userEmail,verifyLink) { // can be also used to change email
     var transporter = nodemailer.createTransport({
         service: process.env.EMAIL_SERVICE,
         auth: {
@@ -88,6 +88,54 @@ function sendEmailNewsletter([userEmail],newsletterText) {
         to: [userEmail],
         subject: 'PLX  - newsletter'+ new Date().toLocaleDateString() ,
         text: newsletterText // todo fancy text here
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      }); 
+}
+function sendEmailNewMessage(userEmail,adTitle) { 
+    var transporter = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_USER
+        }
+      });
+      
+      var mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: userEmail,
+        subject: 'PLX  - masz nową wiadomość dotyczącą ogłoszenia ' +adTitle,
+        text: 'Zaloguj się i zobacz bo nie chce mi sie robic direct linkow to sobie wejdz na strone i sie zaloguj' // todo fancy text here
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      }); 
+}
+function sendEmailStarredCategory(userEmail,starredCategory) { //notify users about starred categories, just to let em know when to buy new iphone etc 
+    var transporter = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_USER
+        }
+      });
+      
+      var mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: userEmail,
+        subject: 'PLX  - pojawił się nowy przedmiot z kategori ' +starredCategory +' .',
+        text: 'Zaloguj się i kup teraz.' // todo fancy text here
       };
       
       transporter.sendMail(mailOptions, function(error, info){
