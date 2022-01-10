aff876bec9cd118730c9b3132318a385fab3944e
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2022-01-10 18:17:17 +0100

    Update messages.js
    Aktualizacja modelu wiadomości do stanu działającego.

--- models/messages.js
+++ models/messages.js
@@ -3,34 +3,33 @@ const { Schema } = mongoose;
 
 const messageSchema = new Schema({
     ad_id:{  // ad id
-        type: Number,
+        type: String,
         required : true
        },
-    owner_id:{  // id of ad owner
-     type: Number,
-     required : true
-    },
-    owner_messages: { //array of msg
-        type: [String],
-        required : true
-        },
+    // owner_id:{  // id of ad owner
+    //  type: String,
+    //  required : true
+    // },
+    owner_messages: [{ //array of msg
+        type: String,
+        required : false
+        }],
     buyer_id:{  // id of a buyer
-     type: Number,
+     type: String,
      required : true
     },
-    buyer_messages: { //array of msg
-        type: [String],
-        required : true
-        },
-    message_dates:{
-    type: [Date], // date n time of msg
-    default: Date.now,
-    required: true},
+    buyer_messages: [{ //array of msg
+        type: String,
+        required : false
+        }],
+    message_dates:[{
+    type: String, // date n time of msg
+    required: true}],
     is_sold:{//disable messages if user sold the item
         type: Boolean,
         default: false
     }
 });
-let report = mongoose.model("Messages", messageSchema);
+let message = mongoose.model("Messages", messageSchema);
 message.createCollection();
 module.exports = message;
