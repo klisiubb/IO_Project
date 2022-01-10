06e8aca7f184cb2f061a1b148b5aed8c494cc495
BartShoot <33185107+BartShoot@users.noreply.github.com>
2022-01-10 18:12:06 +0100

    aktualizacja schematu ogłoszeń

--- models/ad_schema.js
+++ models/ad_schema.js
@@ -1,43 +1,52 @@
-const mongoose = require('mongoose');
-//mongoose.connect("mongodb://127.0.0.1:27017/users", { useNewUrlParser: true });   
+const mongoose = require("mongoose");
+//mongoose.connect("mongodb://127.0.0.1:27017/users", { useNewUrlParser: true });
 const { Schema } = mongoose;
 
 const adSchema = new Schema({
-    owner_id:{ 
-     type: Number,
-     required : true
-    },
-    title:{  
+  category: {
     type: String,
-    required : true
-    },
-    category_id:{
-    type: Number,
-    required : true
-    }, 
-    description: {
-    type: String,
-    required : true
-    },
-  Photo:{
-    type:[String],
-    required: true
+    required: true,
   },
-    phone:   {
-    type:Number,
-    required: true},
-    date:{
-    type: Date,
-    default: Date.now,
-    required: true},
-    is_premium:{
-        type: Boolean, // to check if user paid for more views, main page etc.
-        default: false
-    },
-    views:{
-        type:Number,
-        default:0
-    }
+  userid: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: false,
+  },
+  is_premium: {
+    type: Boolean, // to check if user paid for more views, main page etc.
+    default: false,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
 });
 let ad = mongoose.model("Ad", adSchema);
 ad.createCollection();
