e2183af859063f99a1ff09e6fbfb02148cc60b43
BartShoot <33185107+BartShoot@users.noreply.github.com>
2022-01-10 18:04:40 +0100

    formatowanie daty w profilu

---
+++ functions/dateFormaterProfile.js
@@ -0,0 +1,20 @@
+function formatDate(date){
+    month = date.getMonth()+1;
+    monthString = "";
+     if(month == 1) { monthString = "Stycznia";}
+     if(month == 2) { monthString = "Lutego";}
+     if(month == 3) { monthString = "Marca";}
+     if(month == 4) { monthString = "Kwietnia";}
+     if(month == 5) { monthString = "Maja";}
+     if(month == 6) { monthString = "Czerwca";}
+     if(month == 7) { monthString = "Lipca";}
+     if(month == 8) { monthString = "Sierpnia";}
+     if(month == 9) { monthString = "Września";}
+     if(month == 10) { monthString = "Października";}
+     if(month == 11) { monthString = "Listopada";}
+     if(month == 12) { monthString = "Grudnia";}
+    
+    const formatedDate = String(date.getDate()+" " +monthString +" " + date.getFullYear());
+    return formatedDate;
+    }
+    module.exports = formatDate;
